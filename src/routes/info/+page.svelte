70661b71ef5bb3fa1486ebc78c5a00ed2d3beb26
mfw78 <mfw78@protonmail.com>
2023-07-22 11:30:40 +0000

    feat(svelte-example): use safe store

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,21 +1,7 @@
 <script lang="ts">
 	import CollapsableComponent from '$lib/components/CollapsableComponent.svelte';
 	import Address from '$lib/components/web3/Address.svelte';
-  import { rpc } from '$lib/store/chain';
-  import { safe, safeInfo, signerAddress } from '$lib/store/safe';
-	import { Safe, EthersAdapter } from '@rndlabs/safe-protocol-kit/dist/src/index';
-  import { SafeAppProvider } from '@rndlabs/safe-apps-provider';
-	import { onMount } from 'svelte';
-  import { Signer, ethers, providers } from 'ethers'
-	import { get } from 'svelte/store';
-
-  let safeSdk: Safe | undefined;
-
-  signerAddress.subscribe(async (address) => {
-    if (!address) return;
-    const ethAdaptor = new EthersAdapter({ethers, signerOrProvider: get(rpc)!});
-    safeSdk = await Safe.create({safeAddress: address, ethAdapter: ethAdaptor})
-  });
+  import { safe, signerAddress } from '$lib/store/safe';
 </script>
 
 {#if safe}
@@ -26,11 +12,11 @@
       <strong>Safe Address:</strong>
       <span><Address address={$signerAddress} showExplorer={true} /></span>
     </div>
-    {#if safeSdk}
+    {#if $safe}
     <div class="fixed-info-item">
       <strong>Fallback Handler:</strong>
       <span>
-        {#await safeSdk?.getFallbackHandler()}
+        {#await $safe?.getFallbackHandler()}
           Loading
         {:then handler}
           <Address address={handler} showExplorer={true} resolveEns={false} />
