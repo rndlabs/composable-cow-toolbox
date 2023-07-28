a9bbdda8f86d365c5d43542ac52b2d1372a6aaf7
mfw78 <mfw78@protonmail.com>
2023-07-28 10:09:05 +0000

    feat(svelte-example): explorer url feat

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
+import { SupportedChainId } from "@cowprotocol/cow-sdk";
+
 // returns a shortened address string with ellipses in the middle
 export const formatAddress = (address: string, chars = 4) => {
 	const start = address.slice(0, chars + 2);
 	const end = address.slice(-chars);
 	return `${start}...${end}`;
 };
+
+export const explorerUrlOrder = (tx: string, chainId?: SupportedChainId) => {
+	switch (chainId) {
+		case SupportedChainId.MAINNET:
+			return `https://explorer.cow.fi/orders/${tx}`;
+		case SupportedChainId.GOERLI:
+			return `https://explorer.cow.fi/goerli/orders/${tx}`;
+		case SupportedChainId.GNOSIS_CHAIN:
+			return `https://explorer.cow.fi/gnosis/orders/${tx}`;
+		default:
+			return `https://explorer.cow.fi/orders/${tx}`;
+	}
+}
